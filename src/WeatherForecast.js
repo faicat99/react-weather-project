3741b61f8aca996e819f2562f44d233f8d9e3ffb
Fainche <faicatmck@gmail.com>
2024-09-08 15:59:32 +0100

    accidentally deleted function handleResponse in WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -7,6 +7,11 @@ export default function WeatherForecast(props){
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
 
+    function handleResponse(response) {
+        setForecast(response.data.daily);
+        setLoaded(true);
+    }
+
     if (loaded) {
         return (
             <div className="WeatherForecast">
